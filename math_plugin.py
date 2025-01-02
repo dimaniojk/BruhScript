0af5e1e9c0b0995d7bbbd7cf2f05fac639aa21ef
Deividas Juris Kaminskis <168298656+dimaniojk@users.noreply.github.com>
2025-01-02 09:00:04 +0200

    Delete math_plugin.py

--- math_plugin.py
+++
@@ -1,7 +0,0 @@
-def math_add(a, b, variables):
-    variables["sum"] = int(a) + int(b)
-    return variables
-
-def math_multiply(a, b, variables):
-    variables["product"] = int(a) * int(b)
-    return variables
