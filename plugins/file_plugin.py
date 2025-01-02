7623636caf7a9fcf1864776a8f6220342ce97b34
Deividas Juris Kaminskis <168298656+dimaniojk@users.noreply.github.com>
2025-01-02 09:03:14 +0200

    Delete plugins directory dublicated

--- plugins/file_plugin.py
+++
@@ -1,9 +0,0 @@
-def file_write(filename, content, variables):
-    with open(filename, "w") as file:
-        file.write(content)
-    return {"status": "success"}
-
-def file_read(filename, variables):
-    with open(filename, "r") as file:
-        content = file.read()
-    return {"file_content": content}
